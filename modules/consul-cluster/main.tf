4d97b2595a1d2262f59bb69d5c974e9caddffd38
sanjaybaskar8049 <sanjaykumar.b1828@gmail.com>
2025-06-26 16:35:57 +0530

    updated launch template lifecycle block

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -143,9 +143,9 @@ resource "aws_launch_template" "launch_template" {
   key_name = var.ssh_key_name
 
   metadata_options {
-     http_tokens                 = "required"               # enforces IMDSv2
-     http_put_response_hop_limit = var.metadata_hop_limit   # default is 1. increase only as needed for your proxies.
-     http_endpoint               = "enabled"                # enables the metadata endpoint
+     http_tokens                 = "required"   # enforces IMDSv2
+     http_put_response_hop_limit = 1            # 
+     http_endpoint               = "enabled"    # enables the metadata endpoint
    }
 
   iam_instance_profile {
@@ -187,6 +187,9 @@ resource "aws_launch_template" "launch_template" {
 
   lifecycle {
     create_before_destroy = true
+    ignore_changes = [
+      latest_version, placement
+    ]
   }
 }
 
